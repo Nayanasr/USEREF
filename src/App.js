25978f8bfefa12bc0d8bb966488a03c9990c73df
Nayana Ramesh <107012@tfcorp.com>
2022-09-08 12:05:54 +0530

    useref files added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import UserefComponent from './components/UserefComponent';
+import ComponentOne from './components/ComponentOne';
+import ComponentTwo from './components/ComponentTwo';
+import UseRef from './components/UseRef';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UserefComponent /> */}
+      {/* <ComponentOne />   */}
+      {/* <ComponentTwo /> */}
+      <UseRef />
+    </div>
+  );
+}
+
+export default App;
